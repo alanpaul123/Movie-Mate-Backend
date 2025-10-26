9903cdf97040a1de4508ba04e74160e0fdd3b458
alanpaul123 <alanpauldxb@gmail.com>
2025-10-26 19:43:34 +0530

    Updated Flask run host for Render

--- app.py
+++ app.py
@@ -210,10 +210,16 @@ def recommend():
 
 # This is used to create the database
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     
+#     with app.app_context():
+#         db.create_all()
+#         print("Database tables created!")
+    
+#     app.run(debug=True, port=5000)
+
+
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-        print("Database tables created!")
-    
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000)
